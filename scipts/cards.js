0b185f84c577a9d21a6d4758f70d810128d5c469
therealProvIV <109302079+therealProvIV@users.noreply.github.com>
2022-11-29 15:05:13 +0300

    2nd review fix

---
+++ scipts/cards.js
@@ -0,0 +1,51 @@
+//Initial Cards
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    },
+    {
+      name: 'Владимир',
+      link: 'https://images.unsplash.com/photo-1589018943871-1cfb223dc7c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+      name: 'Москва',
+      link: 'https://images.unsplash.com/photo-1578632220633-824351c0b694?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+      name: 'Санкт-Петербург',
+      link: 'https://images.unsplash.com/photo-1625564634286-69db27862769?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80'
+    },
+    {
+      name: 'Калининград',
+      link: 'https://images.unsplash.com/photo-1622903741069-e1411285c9fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+      name: 'Челябинск',
+      link: 'https://images.unsplash.com/photo-1589793242094-496c3f75bcea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80'
+    },
+    {
+      name: 'Казань',
+      link: 'https://images.unsplash.com/photo-1592434101771-225a55c5b664?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80' 
+    }
+  ];
--- scipts/index.js
+++ scipts/index.js
@@ -19,80 +19,29 @@ const popupImage = document.querySelector(".popup_image");
 const newImage = document.querySelector(".popup__image");
 const newTitle = document.querySelector(".popup__image-title");
 const initCard = document.querySelector("#first-cards").content;
-const likeClick = (e) => {
+const handleLikeClick = (e) => {
   e.target.classList.toggle('element__heart_active');
 }
 //
 
 
 //Initial Cards Creation
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  },
-  {
-    name: 'Владимир',
-    link: 'https://images.unsplash.com/photo-1589018943871-1cfb223dc7c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-  },
-  {
-    name: 'Москва',
-    link: 'https://images.unsplash.com/photo-1578632220633-824351c0b694?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-  },
-  {
-    name: 'Санкт-Петербург',
-    link: 'https://images.unsplash.com/photo-1625564634286-69db27862769?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80'
-  },
-  {
-    name: 'Калининград',
-    link: 'https://images.unsplash.com/photo-1622903741069-e1411285c9fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-  },
-  {
-    name: 'Челябинск',
-    link: 'https://images.unsplash.com/photo-1589793242094-496c3f75bcea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80'
-  },
-  {
-    name: 'Казань',
-    link: 'https://images.unsplash.com/photo-1592434101771-225a55c5b664?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80' 
-  }
-];
-
 function createCard (name, link) {
-  let Card = initCard.querySelector(".element").cloneNode(true);
-  Card.querySelector(".element__image").src=link;
-  Card.querySelector(".element__text").textContent=name;
-  Card.querySelector(".element__image").alt=name;
-  Card.querySelector(".element__heart").addEventListener('click', likeClick);
-  Card.querySelector(".element__image").addEventListener('click', function() {
+  const card = initCard.querySelector(".element").cloneNode(true);
+  card.querySelector(".element__image").src=link;
+  card.querySelector(".element__text").textContent=name;
+  card.querySelector(".element__image").alt=name;
+  card.querySelector(".element__heart").addEventListener('click', handleLikeClick);
+  card.querySelector(".element__image").addEventListener('click', function() {
     popupImage.classList.add("popup_opened");
     newImage.src=link;
     newImage.alt=name;
     newTitle.textContent=name; 
 })
-  Card.querySelector(".element__bin").addEventListener("click", function() {
-  cardSection.removeChild(Card);
+  card.querySelector(".element__bin").addEventListener("click", function() {
+  cardSection.removeChild(card);
 });
-  return Card;
+  return card;
 };
 
 for (let i=0; i<initialCards.length; i++) {
